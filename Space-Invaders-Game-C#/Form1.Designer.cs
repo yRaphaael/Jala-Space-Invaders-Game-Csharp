5fa108308d0380671d23e1cc734c0e9dce9b7e52
yRaphaael <raphaelgameplays011@gmail.com>
2024-01-24 16:28:29 -0300

    Adicionar arquivos de projeto.

---
+++ Space-Invaders-Game-C#/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Space_Invaders_Game_C_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.player = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.BackColor = System.Drawing.Color.Transparent;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.ForeColor = System.Drawing.Color.White;
+            this.txtScore.Location = new System.Drawing.Point(12, 632);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(100, 25);
+            this.txtScore.TabIndex = 0;
+            this.txtScore.Text = "Score: 0";
+            this.txtScore.Click += new System.EventHandler(this.txtScore_Click);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.mainGameTimerEvent);
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = global::Space_Invaders_Game_C_.Properties.Resources.ship_stand;
+            this.player.Location = new System.Drawing.Point(600, 603);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(66, 66);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            //this.player.Click += new System.EventHandler(this.player_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::Space_Invaders_Game_C_.Properties.Resources.wallpaperflare_com_wallpaper;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.txtScore);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Jala Space Invaders ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox player;
+    }
+}
+
---
+++ Space-Invaders-Game-C#/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Space_Invaders_Game_C_
+{
+    public partial class Form1 : Form
+    {
+
+        bool goLeft, goRight;
+        int playerSpeed = 6;
+        int enemySpeed = 3;
+        int score = 0;
+        int enemyBulletTimer = 300;
+
+        PictureBox[] invadersArray;
+
+        bool shooting;
+        bool isGameOver;
+
+        public Form1()
+        {
+            InitializeComponent();
+            gameSetup();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtScore_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mainGameTimerEvent(object sender, EventArgs e)
+        {
+            txtScore.Text = "Score: " + score;
+            if (goLeft)
+            {
+                player.Left -= playerSpeed;
+            }
+            if (goRight)
+            {
+                player.Left += playerSpeed;
+            }
+
+            enemyBulletTimer -= 10;
+
+            if (enemyBulletTimer < 1)
+            {
+                enemyBulletTimer = 300;
+                makeBullet("invaderBullet");
+            }
+
+            foreach (Control x in this.Controls)
+            {
+
+                if (x is PictureBox && (string)x.Tag == "invaders")
+                {
+                    x.Left += enemySpeed;
+                    if (x.Left > 1280)
+                    {
+                        x.Top += 65;
+                        x.Left = -80;
+                    }
+
+                    if (x.Bounds.IntersectsWith(player.Bounds))
+                    {
+                        gameOver("You've been invaded by the sad invaders, you are now sad!");
+                    }
+
+                    foreach (Control y in this.Controls)
+                    {
+                        if (y is PictureBox && (string)y.Tag == "bullet")
+                        {
+                            if (y.Bounds.IntersectsWith(x.Bounds))
+                            {
+                                this.Controls.Remove(x);
+                                this.Controls.Remove(y);
+                                score += 1;
+                                shooting = false;
+                            }
+                        }
+                    }
+                }
+
+                if (x is PictureBox && (string)x.Tag == "bullet")
+                {
+                    x.Top -= 20;
+
+                    if (x.Top < 15)
+                    {
+                        this.Controls.Remove(x);
+                        shooting = false;
+                    }
+                }
+
+                if (x is PictureBox && (string)x.Tag == "invaderBullet")
+                {
+                    x.Top += 20;
+                    if (x.Top > 720)
+                    {
+                        this.Controls.Remove(x);
+                    }
+
+                    if (x.Bounds.IntersectsWith(player.Bounds))
+                    {
+                        this.Controls.Remove(x);
+                        gameOver("You've been killed by the sad bullet. Now you are sad forever!");
+                    }
+                }
+            }
+
+            if (score > 12)
+            {
+                enemySpeed = 9;
+            }
+
+            if (score == invadersArray.Length)
+            {
+                gameOver("Woohoo Happiness Found, Keep it safe!");
+            }
+
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if(e.KeyCode == Keys.Space && shooting == false)
+            {
+                shooting = true;
+                makeBullet("bullet");
+            }
+            if(e.KeyCode == Keys.Enter && isGameOver == true)
+            {
+                removeAll();
+                gameSetup();
+
+            }
+        }
+
+        private void makeInvaders()
+        {
+            invadersArray = new PictureBox[25];
+
+            int left = 0;
+
+            for(int i = 0; i < invadersArray.Length; i++)
+            {
+
+                invadersArray[i] = new PictureBox();
+                invadersArray[i].Size = new Size(60, 50);
+                invadersArray[i].Image = Properties.Resources.enemy3_stand;
+                invadersArray[i].Top = 5;
+                invadersArray[i].BackColor = Color.Transparent;
+                invadersArray[i].Tag = "invaders";
+                invadersArray[i].Left = left;
+                invadersArray[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                this.Controls.Add(invadersArray[i]);
+                left = left - 80;
+            }
+        }
+
+        private void gameSetup()
+        {
+            txtScore.Text = "Score: 0";
+            score = 0;
+            isGameOver = false;
+
+            enemyBulletTimer = 300;
+            enemySpeed = 5;
+            shooting = false;
+
+            makeInvaders();
+            gameTimer.Start();
+        }
+
+        private void gameOver(string message)
+        {
+            isGameOver = true;
+            gameTimer.Stop();
+            txtScore.Text = "Score: " + score + " " + message;
+        }
+
+        private void removeAll()
+        {
+            foreach (PictureBox i in invadersArray)
+            {
+                this.Controls.Remove(i);
+            }
+
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox)
+                {
+                    if((string)x.Tag == "bullet" || (string)x.Tag == "invaderBullet")
+                    {
+                        this.Controls.Remove(x);
+                    }
+                }
+            }
+        }
+
+        private void makeBullet(string bulletTag)
+        {
+            PictureBox bullet = new PictureBox();
+            bullet.Image = Properties.Resources.ship_attack;
+            bullet.BackColor = Color.Transparent;
+            bullet.SizeMode = PictureBoxSizeMode.StretchImage;
+            bullet.Size = new Size(30, 20);
+            bullet.Tag = bulletTag;
+            bullet.Left = player.Left + player.Width -100 / 2;
+
+
+            if ((string)bulletTag == "bullet")
+            {
+                bullet.Top = player.Top - 20;
+            }
+            else if ((string)bulletTag == "invaderBullet")
+            {
+                bullet.Top = -100;
+            }
+            
+            this.Controls.Add(bullet);
+            bullet.BringToFront();
+        }
+    }
+}
